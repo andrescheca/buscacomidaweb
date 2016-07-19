ebcfeaedcb137a51a30a42301390b8e10e6449ca
Andrés Checa <acheca@dispatchads.com>
2016-07-19 17:05:41 -0500

    Agregar angular

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
-$(function () {
-	console.log('Página cargada');
-});
+angular.module('buscacomida', [])
+
+.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.buscar = '';
+  $scope.locales = [];
+
+  $http.get('data.json')
+    .then(function(response) {
+      console.log(response.data);
+      $scope.locales = response.data.locales;
+    });
+
+  $scope.rango = function (numero) {
+    return new Array(numero);
+  };
+}]);
